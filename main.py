b08c05e1f1eae7b7394da4d1f0ab059046d1945f
Konstantinos Drosos <dean.drossos@gmail.com>
2021-06-12 06:28:41 -0400

    Change background for start game window

--- main.py
+++ main.py
@@ -4,42 +4,36 @@
 
 
 import tkinter as tk
+from tkinter import *
 from game import BattleShip
+import os
 
 
 class Application(tk.Frame):
 
     """This class represents the starting page of the game"""
 
-    def __init__(self, master):
+    def __init__(self, master, image):
         """Initializes application and gets tk.root as parameter"""
         super().__init__(master)
         self._master = master
+        self._background_image = image
         self._username = tk.StringVar()
         self._create_widget()
 
     def _create_widget(self):
         """Creates an input for username"""
-        self._add_background_image('boat_background.png')
         name_label = tk.Label(self._master, text="Username", font=('Arial', 20), fg='#ffffff')
-        name_label.configure(bg="#5D05C8")
+        name_label.configure(bg="#524A35")
         input_box = tk.Entry(self._master, textvariable=self._username, font=('Arial', 10))
         name_label.grid(column=0, row=0, pady=20)
         input_box.grid(column=1, row=0, pady=20)
 
         # Buttons
-        game_button = tk.Button(self.master, text='Start Game', font=('Arial', 15), command=self._start_game)
-        cancel_button = tk.Button(self.master, text='Cancel', font=('Arial', 15), command=self._cancel_game)
+        game_button = tk.Button(self._master, text='Start Game', font=('Arial', 15), command=self._start_game)
+        cancel_button = tk.Button(self._master, text='Cancel', font=('Arial', 15), command=self._cancel_game)
         game_button.grid(column=0, row=1, padx=40, pady=40)
         cancel_button.grid(column=1, row=1, padx=40, pady=40)
-
-    def _add_background_image(self, photo):
-        """
-        Takes the picture location and loads it as a background
-        """
-        background_image = tk.PhotoImage(file=photo)
-        background_label = tk.Label(self._master, image=background_image, height=400, width=400)
-        background_label.place(x=0, y=0)
 
     def _start_game(self):
         """It starts the game"""
@@ -50,8 +44,20 @@ class Application(tk.Frame):
         """Cancels game"""
         self.master.quit()
 
+    def _add_background_image(self):
+        """
+        Returns a photo image file for the background image
+        """
+        base_folder = os.path.dirname(__file__)
+        image_path = os.path.join(base_folder, self._background_image)
+        photo = tk.PhotoImage(file=image_path)
+
+        photo_label = tk.Label(self._master, image=photo)
+        photo_label.place(x=0, y=0, relwidth=1, relheight=1)
+
 
 root = tk.Tk()
 root.geometry('400x400')
-battleship = Application(root)
+root.configure(bg='#524A35')
+battleship = Application(root, 'boat_background.png')
 battleship.mainloop()
